06a1749489f467de1e726da7b9fc3ef94f9f4b64
Engmbakri <140840351+Engmbakri@users.noreply.github.com>
2024-07-19 22:39:50 +0400

    Delete 0x01-ES6_promise directory

--- 0x01-ES6_promise/0-main.js
+++
@@ -1,8 +0,0 @@
-require('@babel/register')({
-  presets: ['@babel/preset-env']
-});
-
-const getResponseFromAPI = require('./0-promise.js').default;
-
-const response = getResponseFromAPI();
-console.log(response instanceof Promise);
